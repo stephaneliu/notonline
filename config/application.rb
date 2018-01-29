718e4fab898f7ed4ea998d6d3bf322d816982a3d
Stephane Liu <sliu@sjliu.com>
2018-01-28 16:17:11 -1000

    Fix reek violations

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Notonline
+  # This class is responsible for intiating and runining application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
